bf018fb982ca28592ccb108a42bdfa5024af2475
Mngxin868 <81151251+Mngxin868@users.noreply.github.com>
2023-03-17 22:17:26 +0800

    Update 2389. 和有限的最长子序列.cpp

--- leetcode/2389. 和有限的最长子序列.cpp
+++ leetcode/2389. 和有限的最长子序列.cpp
@@ -16,3 +16,29 @@ public:
         return ans;
     }
 };
+
+//前缀和+二分查找可以显著提高效率：
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(),nums.end());
+        vector<int> sum;
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<queries.size();i++){
+            int left=0,right=nums.size()-1,mid;
+            while(left<=right){
+                mid=(left+right)/2;
+                if(queries[i]==nums[mid]) break;
+                if(queries[i]>nums[mid]) left=mid+1;
+                else right=mid-1;
+            }
+            if(queries[i]<nums[mid]){   //最后讨论一下，mid表示是要插入的位置
+                queries[i]=mid;
+            }
+            else queries[i]=mid+1;
+        }
+        return queries;
+    }
+};
