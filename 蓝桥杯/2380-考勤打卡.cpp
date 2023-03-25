8b68d1cd6cc46bb7284ea52daf24a194c6f3d51a
Mngxin868 <81151251+Mngxin868@users.noreply.github.com>
2023-03-25 16:17:53 +0800

    Update 2380-考勤打卡.cpp

--- 蓝桥杯/2380-考勤打卡.cpp
+++ 蓝桥杯/2380-考勤打卡.cpp
@@ -18,3 +18,25 @@ int main()
   }
   return 0;
 }
+
+//采用map
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+int main()
+{
+  int n,a;
+  map<int,int> mymap;
+  string str;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    cin>>str>>a;
+    if(mymap.find(a)==mymap.end())
+      mymap.insert({a,1});
+  }
+  for(auto it=mymap.begin();it!=mymap.end();it++){
+    cout<<it->first<<endl;
+  }
+  return 0;
+}
