20fd04f28a7f3ea3e833679a161f4869abb94eb1
Mngxin868 <81151251+Mngxin868@users.noreply.github.com>
2023-08-11 22:47:14 +0800

    Create 0070. 爬楼梯.cpp

---
+++ leetcode/0070. 爬楼梯.cpp
@@ -0,0 +1,13 @@
+//递归会超时
+class Solution {
+public:
+    int climbStairs(int n) {
+        int ans[46] = {0};
+        ans[1] = 1;
+        ans[2] = 2;
+        for(int i=3;i<=n;i++){
+            ans[i] = ans[i-1]+ans[i-2];
+        }
+        return ans[n];
+    }
+};
